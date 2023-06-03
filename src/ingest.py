324d622203eebdde0d1578c40b46f48558692924
Nevio <neviogomez91@gmail.com>
2023-06-02 20:07:38 -0400

    Fix ingestor bug

--- src/ingest.py
+++ src/ingest.py
@@ -40,11 +40,12 @@ class Ingestor:
         :raises ValueError: If the file extension is not supported.
         """
         ext = "." + file_path.rsplit(".", 1)[-1]
-        if ext not in LOADER_MAPPING:
-            raise ValueError(f"Unsupported file extension '{ext}'")
-        loader_class, loader_args = LOADER_MAPPING[ext]
-        loader = loader_class(file_path, **loader_args)
-        return loader.load()[0]
+        if ext in LOADER_MAPPING:
+            loader_class, loader_args = LOADER_MAPPING[ext]
+            loader = loader_class(file_path, **loader_args)
+            return loader.load()[0]
+
+        raise ValueError(f"Unsupported file extension '{ext}'")
 
     def load_documents(self, ignored_files: List[str] = []) -> List[Document]:
         """
@@ -52,36 +53,46 @@ class Ingestor:
         Excludes files in specified ignore folders and only loads files that are above
         a specified file size threshold. Uses multi-processing if file size is above the
         threshold and multi-threading otherwise.
-        
+
         Returns:
             A list of loaded documents.
         """
+        all_files = []
+        for ext in LOADER_MAPPING:
+            all_files.extend(
+                glob.glob(os.path.join(self.cwd, f"**/*{ext}"), recursive=True)
+            )
+        filtered_files = []
+
+        for file_path in all_files:
+            if not any(
+                ignore_folder in file_path for ignore_folder in self.ignore_folders
+            ):
+                filtered_files.append(file_path)
+
+        # Remove files that are already in the vectorstore if it already exists
+        if ignored_files:
+            filtered_files = [
+                file_path for file_path in all_files if file_path not in ignored_files
+            ]
+
         results = []
-        with tqdm(desc="Loading new documents", ncols=80) as pbar:
-            for entry in os.scandir(self.cwd):
-                if entry.is_file():
-                    file_path = entry.path
-                    if any(
-                        ignore_folder in file_path
-                        for ignore_folder in self.ignore_folders
-                    ):
-                        continue
-                    if file_path in ignored_files:
-                        continue
-                    file_size = entry.stat().st_size
-                    if file_size <= self.threshold:
-                        with ThreadPoolExecutor() as executor:
-                            future = executor.submit(
-                                self.load_single_document, file_path
-                            )
-                            results.append(future.result())
-                    else:
-                        with ProcessPoolExecutor() as executor:
-                            future = executor.submit(
-                                self.load_single_document, file_path
-                            )
-                            results.append(future.result())
-                    pbar.update()
+
+        with tqdm(
+            total=len(filtered_files), desc="Loading new documents", ncols=80
+        ) as pbar:
+            for file_path in filtered_files:
+                file_size = os.path.getsize(file_path)
+                if file_size > self.threshold:
+                    with ProcessPoolExecutor() as executor:
+                        future = executor.submit(self.load_single_document, file_path)
+                        results.append(future.result())
+                else:
+                    with ThreadPoolExecutor() as executor:
+                        future = executor.submit(self.load_single_document, file_path)
+                        results.append(future.result())
+                pbar.update()
+
         return results
 
     def split_docs(self, docs_list: List[Document], language: str) -> List[Document]:
@@ -89,7 +100,7 @@ class Ingestor:
         Splits a list of documents into smaller chunks using the RecursiveCharacterTextSplitter
         object. The function takes a list of Document objects and a language string as
         parameters. The function returns a List of Document objects.
-        
+
         :param docs_list: The list of Document objects to be split.
         :type docs_list: List[Document]
         :param language: The language string to be used by the RecursiveCharacterTextSplitter
@@ -98,18 +109,17 @@ class Ingestor:
         :return: A List of Document objects.
         :rtype: List[Document]
         """
-        text_splitter = RecursiveCharacterTextSplitter.from_language(
-            chunk_size=self.chunk_size,
-            chunk_overlap=self.chunk_overlap,
-            language=language,
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
         )
+        text_splitter.from_language(language)
         texts = text_splitter.split_documents(docs_list)
         return texts
 
     def process_documents(self, ignored_files: List[str] = []) -> List[Document]:
         """
         Process documents and split them into smaller chunks of text.
-        
+
         :param ignored_files: A list of files to ignore. Default is an empty list.
         :type ignored_files: List[str]
         :return: A list of Document objects after splitting them into smaller chunks.
@@ -169,7 +179,7 @@ class Ingestor:
         Ingests documents to create embeddings and store them locally in a vectorstore using Chroma. If the vectorstore
         already exists, updates and appends to it. If it doesn't exist, creates a new vectorstore. Prints progress
         messages to the console.
-        
+
         Parameters:
         None
         Returns:
